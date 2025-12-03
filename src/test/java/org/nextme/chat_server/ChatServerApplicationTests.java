bda66b290a55bea32c9fd208f3d2665f4174e8ea
= <hakjun@ihagjun-ui-MacBookAir.local>
2025-12-03 10:16:08 +0900

    first commit

---
+++ src/test/java/org/nextme/chat_server/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nextme.chat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
