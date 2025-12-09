be8333cccb0a9fa58975d14b3abfc66c6674d322
= <hakjun@ihagjun-ui-MacBookAir.local>
2025-12-09 14:28:29 +0900

    [feat]채팅 서비스 cors 기능 추가 (#2)

--- src/main/java/org/nextme/chat_server/infrastructure/security/SecurityConfig.java
+++ src/main/java/org/nextme/chat_server/infrastructure/security/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 /**
@@ -17,6 +20,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                // CORS 설정 활성화
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .authorizeHttpRequests(auth -> auth
@@ -24,5 +29,30 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 );
         return http.build();
+    }
+
+    // CORS 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        // CORS 설정 객체 생성
+        CorsConfiguration config = new CorsConfiguration();
+
+        // 허용할 Origin(프론트엔드 주소)
+        config.addAllowedOrigin("http://localhost:63342");
+
+        // 허용할 HTTP 메서드
+        config.addAllowedMethod("*"); // GET, POST, OPTIONS 등 모두 허용
+
+        // 허용할 Header
+        config.addAllowedHeader("*"); // Authorization, Content-Type 등
+
+        // 인증정보(Cookie, Authorization) 포함 허용할지 여부
+        config.setAllowCredentials(true);
+
+        // 모든 경로에 이 설정을 적용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
